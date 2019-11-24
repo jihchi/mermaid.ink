2719314ea3520d769990f36013b70b66fb6e2f56
jihchi <achi@987.tw>
2019-11-24 09:50:11 +0800

    Add Prettier as devDependencies

--- test/app.test.js
+++ test/app.test.js
@@ -31,7 +31,7 @@ describe('app', () => {
       '/img/eyJjb2RlIjoiZ3JhcGggVERcbkFbQ2hyaXN0bWFzXSAtLT58R2V0IG1vbmV5fCBCKEdvIHNob3BwaW5nKVxuQiAtLT4gQ3tMZXQgbWUgdGhpbmt9XG5DIC0tPnxPbmV8IERbTGFwdG9wXVxuQyAtLT58VHdvfCBFW2lQaG9uZV1cbkMgLS0-fFRocmVlfCBGW2ZhOmZhLWNhciBDYXJdXG4iLCJtZXJtYWlkIjp7InRoZW1lIjoiZGVmYXVsdCJ9fQ'
     );
     expect(resp.status).toEqual(200);
-		expect(resp.type).toEqual('image/jpeg');
+    expect(resp.type).toEqual('image/jpeg');
     expect(resp.body.length).toBeGreaterThan(10000);
   });
 
