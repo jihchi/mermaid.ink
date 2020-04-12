12cb5aa55219c926cab2eb31ac1b2c7e518dcf77
jihchi <achi@987.tw>
2020-04-12 09:31:57 +0800

    Format all source code

--- test/kitchensink.test.js
+++ test/kitchensink.test.js
@@ -164,7 +164,7 @@ describe('app', () => {
         '/svg/eyJjb2RlIjoiZ3JhcGggVERcbkFbQ2hyaXN0bWFzXSAtLT58R2V0IG1vbmV5fCBCKEdvIHNob3BwaW5nKVxuQiAtLT4gQ3tMZXQgbWUgdGhpbmt9XG5DIC0tPnxPbmV8IERbTGFwdG9wXVxuQyAtLT58VHdvfCBFW2lQaG9uZV1cbkMgLS0-fFRocmVlfCBGW2ZhOmZhLWNhciBDYXJdXG4iLCJtZXJtYWlkIjp7InRoZW1lIjoiZGVmYXVsdCJ9fQ'
       );
       expect(resp.status).toEqual(200);
-			expect(resp.type).toEqual('image/svg+xml');
+      expect(resp.type).toEqual('image/svg+xml');
       expect(resp.body.length).toBeGreaterThan(15 * KB);
     });
 
