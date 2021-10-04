9528d5b533288b3dc774666b5ac570830ebd3fb1
Lucy Linder <lucy.derlin@gmail.com>
2021-10-04 19:42:32 +0200

    Reset yarn.lock and remove debug leftover

--- src/views/svg.js
+++ src/views/svg.js
@@ -7,7 +7,6 @@ const svg = async (ctx, page) => {
   const svg = await page.$eval('#container > svg', (e) =>
     // this is a work-around by @hat215, for more details:
     // https://github.com/mermaid-js/mermaid-live-editor/issues/26#issuecomment-667678228
-    //e.style.background = 'red';
     e.outerHTML.replace(/<br>/gi, '<br/>')
   );
   debug('got the svg element, file size: %o', svg.length);
