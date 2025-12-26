252e3aba66a2f3923ebf52164328b4b59c5a3b0e
jihchi <achi@987.tw>
2025-12-26 09:16:14 +0200

    Deprecate `/services/oembed` route

--- test/kitchensink.test.js
+++ test/kitchensink.test.js
@@ -28,68 +28,6 @@ describe('app', () => {
     expect(resp.charset).toEqual('utf-8');
   });
 
-  describe('GET /services/oembed', () => {
-    test('returns 400 when url is missing', async () => {
-      const resp = await request.get('/services/oembed');
-      expect(resp.status).toEqual(400);
-      expect(resp.text).toMatchInlineSnapshot(`"query \"url\" is required"`);
-    });
-
-    test('returns 404 when url is invalid', async () => {
-      const url = encodeURIComponent('abc');
-      const resp = await request.get(`/services/oembed?url=${url}`);
-      expect(resp.status).toEqual(404);
-      expect(resp.text).toMatchInlineSnapshot(`"Error: Invalid URL"`);
-    });
-
-    test('returns 404 when protocol is unsupported', async () => {
-      const url = encodeURIComponent('http://mermaid.ink');
-      const resp = await request.get(`/services/oembed?url=${url}`);
-      expect(resp.status).toEqual(404);
-      expect(resp.text).toMatchInlineSnapshot(
-        `"Error: URL protocol supported: https"`
-      );
-    });
-
-    test('returns 404 when hostname is unsupported', async () => {
-      const url = encodeURIComponent('https://github.com');
-      const resp = await request.get(`/services/oembed?url=${url}`);
-      expect(resp.status).toEqual(404);
-      expect(resp.text).toMatchInlineSnapshot(
-        `"Error: URL hostname supported: mermaid.ink"`
-      );
-    });
-
-    test('returns 404 when pathname is unsupported', async () => {
-      const url = encodeURIComponent('https://mermaid.ink/foo/bar');
-      const resp = await request.get(`/services/oembed?url=${url}`);
-      expect(resp.status).toEqual(404);
-      expect(resp.text).toMatchInlineSnapshot(
-        `"Error: URL pathname supported: /img/:code, /svg/:code"`
-      );
-    });
-
-    test('returns correct response', async () => {
-      const url = encodeURIComponent(
-        'https://mermaid.ink/img/eyJjb2RlIjoiZ3JhcGggVERcbiAgQVtBXSAtLT4gQihCKVxuXHRcdCIsIm1lcm1haWQiOnsidGhlbWUiOiJkZWZhdWx0In19'
-      );
-      const resp = await request.get(`/services/oembed?url=${url}`);
-
-      expect(resp.status).toEqual(200);
-      expect(resp.type).toEqual('application/json');
-      expect(resp.charset).toEqual('utf-8');
-      expect(resp.body).toMatchObject({
-        height: expect.any(Number),
-        provider_name: 'Mermaid Ink',
-        provider_url: 'https://mermaid.ink',
-        type: 'photo',
-        url: 'https://mermaid.ink/img/eyJjb2RlIjoiZ3JhcGggVERcbiAgQVtBXSAtLT4gQihCKVxuXHRcdCIsIm1lcm1haWQiOnsidGhlbWUiOiJkZWZhdWx0In19',
-        version: '1.0',
-        width: expect.any(Number),
-      });
-    });
-  });
-
   describe('/img', () => {
     test('returns 404 when there is no code provided', async () => {
       const resp = await request.get('/img');
